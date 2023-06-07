b8c974d72baee6918de4f5becbc90e7b3a142174
Mert Kantaroglu <mertkantaroglu@gmail.com>
2023-06-07 14:57:26 +0300

    Update files for food functionalities

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  resources :foods
   devise_for :users
   root 'foods#index'
 
   get '/shopping_lists', to: 'shopping_lists#index'
 
-  resources :users
-  resources :foods, only: %i[index new create destroy]
+  resources :users, only: [:index, :show] do
+    resources :foods, only: %i[index new create destroy]
+  end
+
   resources :recipes, only: %i[index show new create destroy] do
     resources :recipe_foods
   end
