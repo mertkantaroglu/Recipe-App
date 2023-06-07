8de6c79922f530797070d0df5c61ec5c6979a566
Mert Kantaroglu <mertkantaroglu@gmail.com>
2023-06-07 14:56:07 +0300

    Add food controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,3 +1,62 @@
 class FoodsController < ApplicationController
-  def index; end
+  before_action :set_food, only: %i[ show edit update destroy ]
+
+  # GET /foods or /foods.json
+  def index
+    @user = current_user
+    @foods = current_user.foods
+  end
+
+  # # GET /foods/1 or /foods/1.json
+  def show
+    @food = Food.find(params[:id])
+  end
+
+  # GET /foods/new
+  def new
+    @user = current_user
+    @food = Food.new
+  end
+
+  # POST /foods or /foods.json
+  def create
+    @food = Food.new(food_params)
+    @food.user_id = current_user.id
+
+    respond_to do |format|
+      if @food.save
+        format.html { redirect_to food_url(@food), notice: "Food was successfully created." }
+        format.json { render :show, status: :created, location: @food }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @food.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /foods/1 or /foods/1.json
+  def destroy
+    @food = Food.find(params[:id])
+
+    respond_to do |format|
+
+      if @food.destroy
+        format.html { redirect_to foods_url, notice: "Food was successfully destroyed." }
+        format.json { head :no_content }
+      else
+        format.html { render :new, alert: 'An error has occured while deleting food'}
+      end
+    end
+  end
+
+  private
+
+  def set_food
+    @food = Food.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 end
