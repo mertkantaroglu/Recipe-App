51fe7d1e19627218fe701f29502b84239f236229
jonas-45 <jonaswullah@gmail.com>
2023-06-09 05:51:46 +0000

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,20 @@ Rails.application.routes.draw do
 
   get '/shopping_lists', to: 'shopping_lists#index'
 
+  patch 'recipes/:id/update_public', to: 'recipes#update_public', as: 'recipe_update_public'
+  # get '/public_recipes', to: 'public_recipes#index', as: 'public_recipes'
+
+
   resources :users, only: %i[index show] do
     resources :foods, only: %i[index new create destroy]
   end
 
   resources :recipes, only: %i[index show new create destroy] do
-    resources :recipe_foods
+    # resources :recipe_foods
+    patch :update_public, on: :member
   end
 
   resources :public_recipes, only: [:index]
   resources :general_shopping_list, only: [:index]
+  resources :recipe_foods, only: [ :create, :new, :destroy], path: 'recipe_foods'
 end
